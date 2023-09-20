7e59bd91af488db0297ca5f453d20417133f1453
Denizz325 <09aq.abv@gmail.com>
2023-09-20 21:20:17 +0300

    updated

--- Phyton Fundamentals/Homework and labs/02data_type_and_variables/05special_number.py
+++ Phyton Fundamentals/Homework and labs/02data_type_and_variables/05special_number.py
@@ -1,14 +1,13 @@
 number = int(input())
 
-for current_number in range(1, number):
-    current_number_as_str = str(current_number)
-    digit_sum = 0
+for current_number in range(1, number + 1):
+    current_number_as_string = str(current_number)
+    digits_sum = 0
 
-    for digit in current_number_as_str:
-        digit_sum += int(digit)
-
+    for digit in current_number_as_string:
+        digits_sum += int(digit)
     is_special = False
 
-    if digit_sum == 5 or digit_sum == 7 or digit_sum == 11:
+    if digits_sum == 5 or digits_sum == 7 or digits_sum == 11:
         is_special = True
     print(f"{current_number} -> {is_special}")
