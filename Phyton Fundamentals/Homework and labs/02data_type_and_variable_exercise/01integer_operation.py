c4ed9abdb9514f66fd22d711212437b7896dadfc
Denizz325 <09aq.abv@gmail.com>
2023-09-21 18:50:45 +0300

    updated

---
+++ Phyton Fundamentals/Homework and labs/02data_type_and_variable_exercise/01integer_operation.py
@@ -0,0 +1,10 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+fourth_number = int(input())
+
+operation_1 = (first_number + second_number)
+operation_2 = operation_1 // third_number
+operation_3 = operation_2 * fourth_number
+
+print(operation_3)
