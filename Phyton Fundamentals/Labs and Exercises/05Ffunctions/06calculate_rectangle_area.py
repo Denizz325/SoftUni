f0319e99e7111ad58b4b9b025e4fdc0d695e872f
Denizz325 <09aq.abv@gmail.com>
2023-10-04 20:46:06 +0300

    updated

---
+++ Phyton Fundamentals/Labs and Exercises/05Ffunctions/06calculate_rectangle_area.py
@@ -0,0 +1,10 @@
+def rectangle_area(width, height):
+    result = width * height
+    return result
+
+result = 0
+
+width = int(input())
+height = int(input())
+
+print(rectangle_area(width, height))
