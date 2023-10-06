224c8ae37325412dfdb5b3ab1e23e1eeb378ea8c
Denizz325 <09aq.abv@gmail.com>
2023-10-06 19:34:15 +0300

    updated

---
+++ Phyton Fundamentals/Labs and Exercises/05functions_exercise/01smallest_of_three_numbers.py
@@ -0,0 +1,11 @@
+def smallest(some_numbers:list)  -> int:
+    return min(some_numbers)
+
+
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+
+smallest_number = smallest([first_num, second_num, third_num])
+
+print(smallest_number)
