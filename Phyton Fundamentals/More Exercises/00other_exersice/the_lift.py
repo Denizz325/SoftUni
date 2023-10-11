051432fae75611dd542c5c4fdb998d212626c885
Denizz325 <09aq.abv@gmail.com>
2023-10-11 20:15:43 +0300

    updated

--- Phyton Fundamentals/More Exercises/00other_exersice/the_lift.py
+++ Phyton Fundamentals/More Exercises/00other_exersice/the_lift.py
@@ -1,1 +1,12 @@
 # 01. Programming Fundamentals Mid Exam Retake задача 2
+
+waiting_people = int(input())
+current_people_on_lift = list(map(int, input().split()))
+
+while waiting_people != 0:
+
+    for current_lift in :
+        if current_people_on_lift == 4:
+            break
+        current_lift += 1
+        waiting_people -= current_lift
