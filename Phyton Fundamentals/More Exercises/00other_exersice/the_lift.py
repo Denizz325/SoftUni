8c3b8935a66e801cecd2205f84bc26445c422b09
Denizz325 <09aq.abv@gmail.com>
2023-10-12 20:19:34 +0300

    updated

--- Phyton Fundamentals/More Exercises/00other_exersice/the_lift.py
+++ Phyton Fundamentals/More Exercises/00other_exersice/the_lift.py
@@ -1,12 +1,27 @@
 # 01. Programming Fundamentals Mid Exam Retake задача 2
 
-waiting_people = int(input())
-current_people_on_lift = list(map(int, input().split()))
+people_waiting = int(input())
+lift_state = list(map(int, input().split()))
 
-while waiting_people != 0:
+for wagon_position, current_wagon in enumerate(lift_state):
+    available_space = 4 - current_wagon
+    if available_space > 0:
+        if people_waiting >= available_space:
+            lift_state[wagon_position] = 4
+            people_waiting -= available_space
+            available_space = 0
+        else:
+            lift_state[wagon_position] += people_waiting
+            people_waiting = 0
 
-    for current_lift in :
-        if current_people_on_lift == 4:
-            break
-        current_lift += 1
-        waiting_people -= current_lift
+if people_waiting == 0 and available_space == 0:
+    print(*lift_state)
+
+elif sum(lift_state) < len(lift_state) * 4:
+    print("The lift has empty spots!")
+    print(*lift_state)
+else:
+    print(f"There isn't enough space! {people_waiting} people in a queue!")
+    print(*lift_state)
+
+
