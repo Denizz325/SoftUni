834b2f06276df3673ed8fa152d9d0c281b429ba8
Denizz325 <09aq.abv@gmail.com>
2023-10-12 19:40:40 +0300

    updated

--- Phyton Fundamentals/Labs and Exercises/07list_advanced/03to_do_list.py
+++ Phyton Fundamentals/Labs and Exercises/07list_advanced/03to_do_list.py
@@ -1,17 +1,17 @@
-def process_todo_note():
+def process_todo_notes():
     todo_notes = []
 
     while True:
         note = input()
+
         if note == "End":
             break
 
         todo_notes.append(note)
 
-        sorted_notes = sorted(todo_notes, key=lambda x: int(x.split('-')[0]))
-        return [note.split('-')[1] for note in sorted_notes]
+    sorted_notes = sorted(todo_notes, key=lambda x: int(x.split('-')[0]))
+    return [note.split('-')[1] for note in sorted_notes]
 
 
-result = process_todo_note()
-
+result = process_todo_notes()
 print(result)
