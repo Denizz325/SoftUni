9b92e851d91df4972259f00adafcd859ed5a5c6e
Denizz325 <09aq.abv@gmail.com>
2023-10-14 17:03:30 +0300

    updated

--- Phyton Fundamentals/Labs and Exercises/08list_advanced_exercise/07group_of_10's.py
+++ Phyton Fundamentals/Labs and Exercises/08list_advanced_exercise/07group_of_10's.py
@@ -1,1 +1,8 @@
-list_of_number = [int(num) for num in input().split(", ")]
+list_of_numbers = [int(num) for num in input().split(", ")]
+current_group = 10
+while list_of_numbers:
+    filtered_numbers_for_current_group = [number for number in list_of_numbers if number <= current_group]
+    print(f"Group of {current_group}'s: {filtered_numbers_for_current_group}")
+    current_group += 10
+    list_of_numbers = [number for number in list_of_numbers if number not in filtered_numbers_for_current_group]
+
