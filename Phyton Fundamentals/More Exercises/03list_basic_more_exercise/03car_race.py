565bf0a6dfdbe3fc284654d54350f339f086e8f2
Denizz325 <09aq.abv@gmail.com>
2023-10-15 11:23:27 +0300

    updated

--- Phyton Fundamentals/More Exercises/03list_basic_more_exercise/03car_race.py
+++ Phyton Fundamentals/More Exercises/03list_basic_more_exercise/03car_race.py
@@ -1,7 +1,7 @@
 numbers = [int(num) for num in input().split()]
 finish_line_index = len(numbers) // 2
 left_car = numbers[:finish_line_index]
-right_car = numbers[finish_line_index + 1:]
+right_car = numbers[len(numbers): finish_line_index: -1]
 
 total_left_car_time = 0
 total_right_car_time = 0
@@ -9,7 +9,6 @@ total_right_car_time = 0
 for current_time in left_car:
     if current_time == 0:
         total_left_car_time *= 0.8
-
     total_left_car_time += current_time
 
 for current_time in right_car:
@@ -19,8 +18,9 @@ for current_time in right_car:
 
 if total_right_car_time < total_left_car_time:
     print(f"The winner is {'right'} with total time: {total_right_car_time:.1f}")
-else:
+elif total_right_car_time > total_left_car_time:
     print(f"The winner is {'left'} with total time: {total_left_car_time:.1f}")
+
 
 
 
