2c25e69f231b2e8e28ad360e0f70dd615b95dd39
Denizz325 <09aq.abv@gmail.com>
2023-10-15 10:43:25 +0300

    updated

--- Phyton Fundamentals/Labs and Exercises/08list_advanced_exercise/010pokemon_don't_go.py
+++ Phyton Fundamentals/Labs and Exercises/08list_advanced_exercise/010pokemon_don't_go.py
@@ -6,23 +6,36 @@ while True:
     index = int(input())
     if index < 0:
         index = 0
+        popped_value = numbers.pop(index)
+        list_with_popped_nums.append(popped_value)
+        if index < 0:
+            last_element = numbers[-1]
+            numbers.insert(0, last_element)
+        for i in range(len(numbers)):
+            if numbers[i] <= popped_value:
+                numbers[i] += popped_value
+            else:
+                numbers[i] -= popped_value
     elif index > len(numbers) - 1:
         index = len(numbers) - 1
-
-    popped_value = numbers.pop(index)
-    if index > len(numbers) - 1:
-        first_element = numbers[0]
-        numbers.append(first_element)
-    if index < 0:
-        last_element = numbers[-1]
-        numbers.insert(0, last_element)
-    list_with_popped_nums.append(popped_value)
-
-    for i in range(len(numbers)):
-        if numbers[i] <= popped_value:
-            numbers[i] += popped_value
-        else:
-            numbers[i] -= popped_value
+        popped_value = numbers.pop(index)
+        list_with_popped_nums.append(popped_value)
+        if index > len(numbers) - 1:
+            first_element = numbers[0]
+            numbers.append(first_element)
+            for i in range(len(numbers)):
+                if numbers[i] <= popped_value:
+                    numbers[i] += popped_value
+                else:
+                    numbers[i] -= popped_value
+    else:
+        popped_value = numbers.pop(index)
+        list_with_popped_nums.append(popped_value)
+        for i in range(len(numbers)):
+            if numbers[i] <= popped_value:
+                numbers[i] += popped_value
+            else:
+                numbers[i] -= popped_value
 
 
 
