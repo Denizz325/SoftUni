12bc094c92a81ba4afe889406b562e7b58a72d8b
Denizz325 <09aq.abv@gmail.com>
2023-10-22 11:15:13 +0300

    updated

--- Phyton Fundamentals/Exams/regular_mid_еxam/03school_library.py
+++ Phyton Fundamentals/Exams/regular_mid_еxam/03school_library.py
@@ -1,27 +1,33 @@
 book_shelf = input().split("&")
 
-command = input().split(" | ")
 
-while command[0] == 'Done':
+while True:
+    command = input().split(" | ")
+    if command[0] == 'Done':
+        break
     action = command[0]
 
     if action == "Add Book":
         book_name = command[1]
         if book_name not in book_shelf:
             book_shelf.insert(0, book_name)
-    if action == "Take Book":
+    elif action == "Take Book":
         book_name = command[1]
         if book_name in book_shelf:
             book_shelf.remove(book_name)
-    if action == "Swap Book":
-        old_book = command[1]
-        new_book = command[2]
-        book_shelf[old_book] = new_book
-    if action == "Insert Book":
+    elif action == "Swap Books":
+        book_1 = command[1]
+        book_2 = command[2]
+        if book_1 in book_shelf and book_2 in book_shelf:
+            index1 = book_shelf.index(book_1)
+            index2 = book_shelf.index(book_2)
+            book_shelf[index1], book_shelf[index2] = book_shelf[index2], book_shelf[index1]
+
+    elif action == "Insert Book":
         book_name = command[1]
         if book_name not in book_shelf:
             book_shelf.append(book_name)
-    if action == "Check Book":
+    elif action == "Check Book":
         index_to_check = int(command[1])
         if 0 <= index_to_check < len(book_shelf):
             print(book_shelf[index_to_check])
