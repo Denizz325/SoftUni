e6e57765a88104aaef26b0c31deea9cd10c29504
Denizz325 <09aq.abv@gmail.com>
2023-10-31 19:22:54 +0200

    updated

--- Phyton Fundamentals/Labs and Exercises/011dictionaries/01bakery.py
+++ Phyton Fundamentals/Labs and Exercises/011dictionaries/01bakery.py
@@ -4,6 +4,6 @@ bakery = {}
 for i in range(0, len(table_of_stock), 2):
     food = table_of_stock[i]
     quantity = table_of_stock[i + 1]
-    bakery[key] = int(value)
+    bakery[food] = int(quantity)
 
 print(bakery)
