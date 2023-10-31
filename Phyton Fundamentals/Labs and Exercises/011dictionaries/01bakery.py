f5d52c379c963bbaa8d06ed3f1d31ea8e9e58407
Denizz325 <09aq.abv@gmail.com>
2023-10-31 19:22:35 +0200

    updated

--- Phyton Fundamentals/Labs and Exercises/011dictionaries/01bakery.py
+++ Phyton Fundamentals/Labs and Exercises/011dictionaries/01bakery.py
@@ -2,8 +2,8 @@ table_of_stock = input().split()
 bakery = {}
 
 for i in range(0, len(table_of_stock), 2):
-    key = table_of_stock[i]
-    value = table_of_stock[i + 1]
+    food = table_of_stock[i]
+    quantity = table_of_stock[i + 1]
     bakery[key] = int(value)
 
 print(bakery)
