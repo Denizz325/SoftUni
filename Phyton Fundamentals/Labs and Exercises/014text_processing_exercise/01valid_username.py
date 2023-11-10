db00c9240ba847c6c6d29e9e8f2b189ef5052ff1
Denizz325 <09aq.abv@gmail.com>
2023-11-10 22:20:00 +0200

    updated

--- Phyton Fundamentals/Labs and Exercises/014text_processing_exercise/01valid_username.py
+++ Phyton Fundamentals/Labs and Exercises/014text_processing_exercise/01valid_username.py
@@ -1,7 +1,33 @@
-text = input().split(", ")
+def length_is_valid(some_username):
+    if 3 <= len(some_username) <= 16:
+        return True
+    return False
 
-for password in text:
-    if 3 <= len(password) <= 16:
+
+def characters_are_valid(some_username):
+    for character in some_username:
+        if not (character.isalnum() \
+                or character == "-" \
+                or character == "_"):
+            return False
+    return True
 
 
+def no_redundant_symbols(some_username):
+    if " " in some_username:
+        return False
+    return True
 
+
+def username_is_valid(some_username):
+    if length_is_valid(some_username) \
+            and characters_are_valid(some_username) \
+            and no_redundant_symbols(some_username):
+        return True
+    return False
+
+
+usernames = input().split(", ")
+for username in usernames:
+    if username_is_valid(username):
+        print(username)
