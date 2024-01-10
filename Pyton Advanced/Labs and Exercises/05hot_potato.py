b1bbdc24f41e0bf10dcfb216bb765294c0330db6
Denizz325 <09aq.abv@gmail.com>
2024-01-10 15:55:31 +0200

    updated

--- Pyton Advanced/Labs and Exercises/05hot_potato.py
+++ Pyton Advanced/Labs and Exercises/05hot_potato.py
@@ -1,2 +1,11 @@
 from collections import deque
 
+names = deque(input().split())
+num = int(input()) - 1
+
+while len(names) > 1:
+    names.rotate(-num)
+    removed_kid = names.popleft()
+    print(f"Removed {removed_kid}")
+
+print(f"Last is {names.popleft()}")
