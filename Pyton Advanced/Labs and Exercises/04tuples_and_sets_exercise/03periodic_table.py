ab6b87766650f7c1aa98bab03ff32957d82bc74f
Denizz325 <09aq.abv@gmail.com>
2024-01-17 13:12:13 +0200

    updated

--- Pyton Advanced/Labs and Exercises/04tuples_and_sets_exercise/03periodic_table.py
+++ Pyton Advanced/Labs and Exercises/04tuples_and_sets_exercise/03periodic_table.py
@@ -1,3 +1,13 @@
+n = int(input())
+unique_elements_set = set()
+
+for i in range(n):
+    compounds = input().split()
+    unique_elements_set.update(compounds)
+
+
+for element in unique_elements_set:
+    print(element)
 
 
 
