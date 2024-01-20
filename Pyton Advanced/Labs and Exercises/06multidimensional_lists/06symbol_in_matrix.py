95491f01555af49460332f626f7895bd6bbdda1d
Denizz325 <09aq.abv@gmail.com>
2024-01-20 13:26:52 +0200

    updated

--- Pyton Advanced/Labs and Exercises/06multidimensional_lists/06symbol_in_matrix.py
+++ Pyton Advanced/Labs and Exercises/06multidimensional_lists/06symbol_in_matrix.py
@@ -20,5 +20,25 @@ if not is_found:
     print(f"{el_to_search} does not occur in the matrix")
 
 
+#####################2ро решение#################################################
+
+n = int(input())
+matrix = []
+
+for _ in range(n):
+    matrix.append(list(input()))
+
+el_to_search = input()
+
+for row_index in range(n):
+    for col_index in range(len(matrix[row_index])):
+        if matrix[row_index][col_index] == el_to_search:
+            print(f"({row_index}, {col_index})")
+            exit()
+
+print(f"{el_to_search} does not occur in the matrix")
+
+
+
 
 
