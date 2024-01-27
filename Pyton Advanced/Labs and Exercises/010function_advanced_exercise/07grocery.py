530ed9b31f8b6f7b1f2e4613290e0df2889f9500
Denizz325 <09aq.abv@gmail.com>
2024-01-27 12:06:35 +0200

    updated

---
+++ Pyton Advanced/Labs and Exercises/010function_advanced_exercise/07grocery.py
@@ -0,0 +1,10 @@
+def grocery_store(**kwargs):
+    kwargs = sorted(kwargs.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
+
+    result = []
+
+    for product, quantity in kwargs:
+        result.append(f"{product}: {quantity}")
+
+    return "\n".join(result)
+
