4c12296fa5b81da0023727b9d5864393cc67d240
Denizz325 <09aq.abv@gmail.com>
2024-02-25 21:06:24 +0200

    updated

--- Python OOP/Labs and Exercise/02first_steps_in_oop_exercise/04cup.py
+++ Python OOP/Labs and Exercise/02first_steps_in_oop_exercise/04cup.py
@@ -4,4 +4,17 @@ class Cup:
         self.quantity = quantity
 
     def fill(self, quantity):
-        
+        if self.quantity + quantity <= self.size:
+            self.quantity += quantity
+
+
+    def status(self):
+        return abs(self.size - self.quantity)
+
+
+cup = Cup(100, 50)
+
+print(cup.status())
+cup.fill(40)
+cup.fill(20)
+print(cup.status())
