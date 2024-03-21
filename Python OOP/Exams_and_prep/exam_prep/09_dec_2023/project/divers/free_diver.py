487584144e672e5991419aa27e91e29ac89667cc
Denizz325 <09aq.abv@gmail.com>
2024-03-21 12:23:45 +0200

    updated

---
+++ Python OOP/Exams_and_prep/exam_prep/09_dec_2023/project/divers/free_diver.py
@@ -0,0 +1,22 @@
+from project.divers.base_diver import BaseDiver
+
+
+class FreeDiver(BaseDiver):
+    OXYGEN_LEVEL = 120
+
+    def __init__(self, name):
+        super().__init__(name, FreeDiver.OXYGEN_LEVEL)
+
+    def miss(self, time_to_catch):
+        if round(self.oxygen_level - (time_to_catch * 0.6)) < 0:
+            self.oxygen_level = 0
+
+        else:
+            self.oxygen_level = round(self.oxygen_level - (time_to_catch * 0.6))
+
+        if self.oxygen_level == 0:
+            self.has_health_issue = True
+
+    def renew_oxy(self):
+        self.oxygen_level = FreeDiver.OXYGEN_LEVEL
+        self.has_health_issue = False
---
+++ Python OOP/Exams_and_prep/exam_prep/09_dec_2023/project/divers/scuba_diver.py
@@ -0,0 +1,20 @@
+from project.divers.base_diver import BaseDiver
+
+
+class ScubaDiver(BaseDiver):
+    OXYGEN_LEVEL = 540
+
+    def __init__(self, name: str):
+        super().__init__(name, ScubaDiver.OXYGEN_LEVEL)
+
+    def miss(self, time_to_catch: int):
+        if round(self.oxygen_level - (time_to_catch * 0.3)) < 0:
+            self.oxygen_level = 0
+        else:
+            self.oxygen_level = round(self.oxygen_level - (time_to_catch * 0.3))
+        if self.oxygen_level == 0:
+            self.has_health_issue = True
+
+    def renew_oxy(self):
+        self.oxygen_level = ScubaDiver.OXYGEN_LEVEL
+        self.has_health_issue = False
